d19bcc3fceca9cb3d6730a9b0b61af7ca8a738f8
Carl Okpala <109344101+Carldtitan@users.noreply.github.com>
2026-01-05 12:46:18 -0800

    Update firebaseClient.js

--- src/services/firebaseClient.js
+++ src/services/firebaseClient.js
@@ -1,13 +1,1 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDYrdy-ALKekM0fC6UQ2JuAXgOrpMMmxZ0",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "bitestate-a77a3.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "bitestate-a77a3",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:403297736948:web:0bf79bff07e4a8e3e626a1",
-};
-
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
